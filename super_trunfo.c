5ff18914c975947bb5c7c29a4e994cba284c6a29
Rafaela Rodrigues <rafaerodriguescontato@gmail.com>
2026-02-27 19:48:39 -0300

    Adiciona super poder e comparacao das cartas

--- super_trunfo.c
+++ super_trunfo.c
@@ -5,12 +5,13 @@ int main(){
      char estado1;
      char codigo1[4];
      char cidade1[50];
-     int populacao1;
+     unsigned long int populacao1;
      float area1;
      float pib1;
      int pontos1;
      float densidade1 = populacao1 / area1;
      float riqueza1 = pib1 / populacao1;
+     float superPoder1 = (float) populacao1 + (float) pontos1 + area1 + pib1 + riqueza1 + (1 \ densidade1);
 
      printf("Cadastro da carta 1\n");
 
@@ -24,7 +25,7 @@ int main(){
 fgets(cidade1, 50, stdin);
 
      printf("Populacao: ");
-     scanf("%d", &populacao1);
+     scanf("%lu", &populacao1);
 
     printf("Area (km2): ");
     scanf("%f", &area1);
@@ -39,12 +40,13 @@ fgets(cidade1, 50, stdin);
     char estado2;
     char codigo2[4];
     char cidade2[50];
-    int populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int pontos2;
     float densidade2 = populacao2 / area2;
     float riqueza2 = pib2 / populacao2;
+    float superPoder2 = (float) populacao2 + (float) pontos2 + area2 + pib2 + riqueza2 + (1 \ densidade1);
 
     printf("Cadastro da carta 2\n");
 
@@ -58,7 +60,7 @@ fgets(cidade1, 50, stdin);
 fgets(cidade1, 50, stdin);
 
     printf("Populacao: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Area (km2): ");
     scanf("%f", &area2);
@@ -69,13 +71,29 @@ fgets(cidade1, 50, stdin);
     printf("Numero de pontos turisticos: ");
     scanf("%d", &pontos2);
 
-    printf("\n\nCartas cadastradas\n");
+    int rPop, rArea, rPib, rPontos, rRiqueza, rDensidade, rSuper;
+
+    rPop = populacao1 > populacao2;
+
+    rArea = area1 > area2;
+
+    rPib = pib1 > pib2;
+
+    rPontos = pontos1 > pontos2;
+
+    rRiqueza = riqueza11 > riqueza2;
+
+    rDensidade = densidade1 < densidade2;
+
+    rSuper = superPoder1 > superPoder2;
+
+    printf("\n\nCARTAS CADASTRADAS\n");
 
       printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", codigo1);
     printf("Cidade: %s\n", cidade1);
-    printf("Populacao: %d\n", populacao1);
+    printf("Populacao: %lu\n", populacao1);
     printf("Area: %.2f km2\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos turisticos: %d\n", pontos1);
@@ -87,11 +105,22 @@ fgets(cidade1, 50, stdin);
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
     printf("Cidade: %s\n", cidade2);
-    printf("Populacao: %d\n", populacao2);
+    printf("Populacao: %lu\n", populacao2);
     printf("Area: %.2f km2\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Pontos turisticos: %d\n", pontos2);
     printf("Densidade Populacional: %.2f hab/km2\n", densidade2);
       printf("PIB per Capita: %.2f reais\n", riqueza2);
-    return 0;
+   
+    printf("\nRESULTADO DAS COMPARACOES\n");
+
+    printf("Populacao: %d\n", rPop);
+    printf("Area: %d\n", rArea);
+    printf("PIB: %d\n", rPib);
+    printf("Pontos Turisticos: %d\n", rPontos);
+    printf("PIB per Capita:: %d\n", rRiqueza);
+    printf("Densidade Populacional: %d\n", rDensidade);
+    printf("Super Poder: %d\n", rSuper);
+
+     return 0;
 }
